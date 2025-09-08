e75d58db6106be6834e679c0f21fde9b2452f018
Mathieu Barbin <mathieu.barbin@gmail.com>
2025-09-08 15:03:07 +0200

    Configure headers in dunolint_base subdirs

--- lib/dunolint_base/src/dune0/compilation_mode.mli
+++ lib/dunolint_base/src/dune0/compilation_mode.mli
@@ -1,3 +1,24 @@
+(*_********************************************************************************)
+(*_  Dunolint - A tool to lint and help manage files in dune projects             *)
+(*_  Copyright (C) 2024-2025 Mathieu Barbin <mathieu.barbin@gmail.com>            *)
+(*_                                                                               *)
+(*_  This file is part of Dunolint.                                               *)
+(*_                                                                               *)
+(*_  Dunolint is free software; you can redistribute it and/or modify it          *)
+(*_  under the terms of the GNU Lesser General Public License as published by     *)
+(*_  the Free Software Foundation either version 3 of the License, or any later   *)
+(*_  version, with the LGPL-3.0 Linking Exception.                                *)
+(*_                                                                               *)
+(*_  Dunolint is distributed in the hope that it will be useful, but WITHOUT      *)
+(*_  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        *)
+(*_  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License  *)
+(*_  and the file `NOTICE.md` at the root of this repository for more details.    *)
+(*_                                                                               *)
+(*_  You should have received a copy of the GNU Lesser General Public License     *)
+(*_  and the LGPL-3.0 Linking Exception along with this library. If not, see      *)
+(*_  <http://www.gnu.org/licenses/> and <https://spdx.org>, respectively.         *)
+(*_********************************************************************************)
+
 type t = Dunolint.Dune.Compilation_mode.t
 
 include module type of Dunolint.Dune.Compilation_mode with type t := t
