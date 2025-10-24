2b82d7ae825aa6db65bb2ff45ce095e538fe7aaa
Mathieu Barbin <mathieu.barbin@gmail.com>
2025-10-24 23:34:46 +0200

    Upgrade to ocamlformat.0.28.1

--- lib/dunolint_cli/src/workspace_root.ml
+++ lib/dunolint_cli/src/workspace_root.ml
@@ -20,37 +20,37 @@
 (*********************************************************************************)
 
 (* This module is derived from the dune code base file
-   * [./bin/workspace_root.mli] which is released under MIT:
-   *
-   * Copyright (c) 2016 Jane Street Group, LLC <opensource@janestreet.com>
-   *
-   * Permission is hereby granted, free of charge, to any person obtaining a copy
-   * of this software and associated documentation files (the "Software"), to deal
-   * in the Software without restriction, including without limitation the rights
-   * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-   * copies of the Software, and to permit persons to whom the Software is
-   * furnished to do so, subject to the following conditions:
-   *
-   * The above copyright notice and this permission notice shall be included in all
-   * copies or substantial portions of the Software.
-   *
-   * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-   * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-   * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-   * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-   * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-   * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-   * SOFTWARE.
-   *
-   *  Changes:
-   *
-   * - Remove explicit dependency to [Stdune] - use [Base] instead.
-   * - Inline source file names (e.g. "dune-workspace").
-   * - Use [Err] instead of [User_message].
-   * - Rename [create_exn] as [find_exn].
-   * - Use polymorphic variant and subtypes for [Kind].
-   * - Remove [reach_from_root_prefix].
-*)
+ * [./bin/workspace_root.mli] which is released under MIT:
+ *
+ * Copyright (c) 2016 Jane Street Group, LLC <opensource@janestreet.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *  Changes:
+ *
+ * - Remove explicit dependency to [Stdune] - use [Base] instead.
+ * - Inline source file names (e.g. "dune-workspace").
+ * - Use [Err] instead of [User_message].
+ * - Rename [create_exn] as [find_exn].
+ * - Use polymorphic variant and subtypes for [Kind].
+ * - Remove [reach_from_root_prefix].
+ *)
 
 module Source_filename = struct
   let dune_project = "dune-project"
