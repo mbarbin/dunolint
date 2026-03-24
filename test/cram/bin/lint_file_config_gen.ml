2a69ccf34dac3d48e23a5965b2b3a7f9b73b6a57
Mathieu Barbin <mathieu.barbin@gmail.com>
2026-03-24 12:56:05 +0100

    Add coverage annotation for post-installed path

--- test/cram/bin/lint_file_config_gen.ml
+++ test/cram/bin/lint_file_config_gen.ml
@@ -68,7 +68,7 @@ let main =
 let version =
   match Build_info.V1.version () with
   | None -> "n/a"
-  | Some v -> Build_info.V1.Version.to_string v
+  | Some v -> Build_info.V1.Version.to_string v [@coverage off]
 ;;
 
 let () = Cmdlang_cmdliner_err_runner.run main ~name:"lint-file-config-gen" ~version
