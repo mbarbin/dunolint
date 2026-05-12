2cd4572c62c7f9779cf44c86d1aadd6689001999
Mathieu Barbin <mathieu.barbin@gmail.com>
2026-05-12 19:07:30 +0200

    Clarify invariant on integers not poly refs

--- src/myers/myers.ml
+++ src/myers/myers.ml
@@ -115,7 +115,7 @@ let compute
       done;
       (* After backtracking, x = y (both equal the length of the initial
          common prefix, which lies on the k=0 diagonal at d=0). *)
-      assert (x = y);
+      assert (Int.equal !x !y);
       while !x > 0 do
         ops := Line.Keep a.(!x - 1) :: !ops;
         decr x
